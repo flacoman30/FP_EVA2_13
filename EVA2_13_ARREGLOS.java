ab9a03087164cc89aaaf5769b7116e1af4541d3d
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-11-06 13:42:51 -0600

    Add files via upload

---
+++ EVA2_13_ARREGLOS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_13_arreglos;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA2_13_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int[10];
+        //int[] --> indica que es un arreglo de enteros
+        // new int[10] -->crea el arreglo con 10 espacios para enteros
+        arreglo[0]=100;//ponemos 100 en el entero en la posicion cero
+        arreglo[9]=900;//ultima posicion (10-1)
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
+        System.out.println(arreglo[6]);
+        System.out.println(arreglo[7]);
+        System.out.println(arreglo[8]);
+        System.out.println(arreglo[9]);
+    }
+    
+}
